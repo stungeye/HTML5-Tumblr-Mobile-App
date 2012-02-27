b8fb2a223e8bafa1d94dbc67235c0f2d23dc734f
Wally Glutton <stungeye@gmail.com>
2012-02-27 14:08:53 -0600

    Tweaking the no click delay code. Not statisfied yet, but it's decent.

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ function strip(html) {
 
 // Hide a DOM element using the CSS class "hidden", unless the class is already present.
 function hide(el) {
-  if (el.className.indexOf('hidden') == -1) {
+  if (el.className.indexOf('hidden') === -1) {
     el.className+= ' hidden';
   }
 }
@@ -22,22 +22,22 @@ function show(el) {
 
 // A callback event for the Android back button. 
 function back_event() {
-  switch_screen('#home')
+  switch_screen('#home');
 }
 
 function switch_screen(id) {
   id = id.substring(1); // Remove the # from the start of the id.
   
+  var current_screen = document.getElementById(id),
+      back_button = document.getElementById('back_button');
+  
   [].forEach.call(document.querySelectorAll("div.screen"), function(el) {
     hide(el);
   });
   
-  current_screen = document.getElementById(id);
   show(current_screen);
   
-  back_button = document.getElementById('back_button');
-  
-  if (id != "home") {
+  if (id !== "home") {
     // We are navigating away from home, so show the home button.
     show(back_button);
     // Bind the Android back button to the back_event callback.
@@ -51,37 +51,37 @@ function switch_screen(id) {
 }
 
 // Helper function the simplify setting click events on elements.
+// The callback_fnc provided should be defined with one parameter,
+// which will be set to the clicked element when executed.
 function set_click(el, callback_fnc) {
-  el.addEventListener('click', function(event) {
-    event.preventDefault();
-    callback_fnc(el); // Hand off the element in case it's required in our callback.
-  }, false);
+  if('ontouchstart' in document.documentElement ) {
+    new NoClickDelay(el, callback_fnc);
+  } else {
+    el.addEventListener('click', function(event) {
+      event.preventDefault();
+      callback_fnc(el); // Hand off the element in case it's required in our callback.
+    }, false);
+  }
 }
 
 // When the DOM is ready we need to set up our various click events.
 document.addEventListener("DOMContentLoaded", function() {
   var main_division   = document.getElementById('main'),
       about_app_link  = document.getElementById('about_app_link'),
-      about_meow_link = document.getElementById('about_meow_link');
+      about_meow_link = document.getElementById('about_meow_link'),
+      back_button = document.getElementById('back_button');
       
   // Prevent webkit from allowing the user to scroll the page using a move gesture.
   // Added to prevent the user from scrolling past the end of the home page.
   main_division.addEventListener('touchmove', function(event) { event.preventDefault(); }, false );
   
   // Set the switch_screen callback for the toolbar back button.
-  back_button = document.getElementById('back_button');
   set_click(back_button, function() { switch_screen('#home'); });
   
   // Bind each of the buttons on the home screen to a switch_screen callback.
   [].forEach.call(document.querySelectorAll("ul.buttons a.switch_screen"), function(el) {
     set_click(el, function(e) { switch_screen(e.getAttribute('href')); });
   });
-  
-  // Make the home screen buttons and toolbar back buttons snappy using noclickdelay.js
-  [].forEach.call(document.querySelectorAll("ul.buttons"), function(el) {
-    new NoClickDelay(el);
-  });
-  new NoClickDelay(back_button);
   
 });
 
@@ -96,7 +96,8 @@ function load_images(json) {
       options,
       instance,
       images = [],
-      posts = json['response']['posts'];
+      posts = json['response']['posts'],
+      launch_link = document.getElementById('launch_link');
 
   // Gather all 20 images returned from the Tumblr API call into an array of hashes.
   for (i = 0; i < posts.length; i++) {
@@ -104,23 +105,23 @@ function load_images(json) {
     photo_big      = photos[0]['url'];
     caption_text   = strip(posts[i]['caption']);
     
-    images.push({ url: photo_big, caption: caption_text })
+    images.push({ url: photo_big, caption: caption_text });
   }
   
   // Configure Photoswipe to load images from the array of hashes created above.
-  options = { enableDrag: false,
-              captionAndToolbarAutoHideDelay: 0,
-              getImageSource: function(obj){
-                return obj.url;
-              },
-              getImageCaption: function(obj){
-                return obj.caption;
-              }
-            }
-            
+  options = {
+    enableDrag: false,
+    captionAndToolbarAutoHideDelay: 0,
+    getImageSource: function(obj){
+      return obj.url;
+    },
+    getImageCaption: function(obj){
+      return obj.caption;
+    }
+  };
+  
   instance = Code.PhotoSwipe.attach(images, options);
   
   // Launch the slideshow when the user clicks on the launch link on the home screen.
-  launch_link = document.getElementById('launch_link');
   set_click(launch_link, function(event) { instance.show(0); });
 }
--- js/noclickdelay.js
+++ js/noclickdelay.js
@@ -2,10 +2,35 @@
 //
 // I changed the touch detection from window.Touch to: 'ontouchstart' in document.documentElement
 // See: http://modernizr.github.com/Modernizr/touch.html
+//
+// I also changed the code such that a callback function is pass into the NoClickDelay function,
+// rather than firing a click event on "theTarget".
+//
+// There still seems to be an issue (present on the Android Galaxy Nexus at least) where a
+// "ghost click" is sometimes fired after the callback has been executed. The onClick
+// handler was added to supress these ghost clicks, although it never appears to be fired.
 
-function NoClickDelay(el) {
-	this.element = typeof el == 'object' ? el : document.getElementById(el);
-	if('ontouchstart' in document.documentElement ) this.element.addEventListener('touchstart', this, false);
+
+// An on-screen debug console.
+// Requires an element with an id of "main".
+function debug_log(msg) {
+  var main = document.getElementById("main"),
+      debug = document.getElementById('glutton_debug');
+  if (debug === null && main !== null) {
+	debug = document.createElement('div');
+	debug.setAttribute('id','glutton_debug');
+	main.appendChild(debug);
+  }
+  if (debug !== null) {
+	debug.innerHTML += "<p>" + msg + "</p>";
+  }
+}
+
+function NoClickDelay(el, callback) {
+	this.element = typeof el === 'object' ? el : document.getElementById(el);
+	this.callback = callback;
+	this.element.addEventListener('touchstart', this, false);
+	this.element.addEventListener('click', this, false);
 }
 
 NoClickDelay.prototype = {
@@ -14,15 +39,20 @@ NoClickDelay.prototype = {
 			case 'touchstart': this.onTouchStart(e); break;
 			case 'touchmove': this.onTouchMove(e); break;
 			case 'touchend': this.onTouchEnd(e); break;
+			case 'click': this.onClick(e); break;
 		}
 	},
 
 	onTouchStart: function(e) {
 		e.preventDefault();
+		e.stopPropagation();
 		this.moved = false;
 
 		this.theTarget = document.elementFromPoint(e.targetTouches[0].clientX, e.targetTouches[0].clientY);
-		if(this.theTarget.nodeType == 3) this.theTarget = theTarget.parentNode;
+		if(this.theTarget.nodeType === 3) {
+			this.theTarget = this.theTarget.parentNode;
+		}
+		
 		this.theTarget.className+= ' pressed';
 
 		this.element.addEventListener('touchmove', this, false);
@@ -35,16 +65,23 @@ NoClickDelay.prototype = {
 	},
 
 	onTouchEnd: function(e) {
+		e.preventDefault();
+		e.stopPropagation();
+		
 		this.element.removeEventListener('touchmove', this, false);
 		this.element.removeEventListener('touchend', this, false);
 
 		if( !this.moved && this.theTarget ) {
 			this.theTarget.className = this.theTarget.className.replace(/ ?pressed/gi, '');
-			var theEvent = document.createEvent('MouseEvents');
-			theEvent.initEvent('click', true, true);
-			this.theTarget.dispatchEvent(theEvent);
+			this.callback(this.theTarget);
 		}
 
 		this.theTarget = undefined;
+	},
+	
+	onClick: function(e) {
+		e.stopPropagation();
+		e.preventDefault();
 	}
+	
 };
